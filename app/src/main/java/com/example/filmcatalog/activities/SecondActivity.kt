7cacf20ed47ba35e217eea3be2c83ccb3ab4fd9e
ilyastuit <ilyastuit@gmail.com>
2020-11-05 21:29:48 +0300

    Enabled view binding

--- app/src/main/java/com/example/filmcatalog/activities/SecondActivity.kt
+++ app/src/main/java/com/example/filmcatalog/activities/SecondActivity.kt
@@ -2,14 +2,13 @@ package com.example.filmcatalog.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.filmcatalog.R
-import com.example.filmcatalog.databinding.ActivityMainBinding
+import com.example.filmcatalog.databinding.ActivitySecondBinding
 
-class MainActivity : AppCompatActivity() {
+class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding =  ActivityMainBinding.inflate(layoutInflater)
+        val binding =  ActivitySecondBinding.inflate(layoutInflater)
         val view = binding.root
 
         setContentView(view)
