08ab3e59a6795ea3c896bbce4fbef864beb46ae8
ilyastuit <ilyastuit@gmail.com>
2020-11-05 22:31:13 +0300

    Added moveToNextScreen method

--- app/src/main/java/com/example/filmcatalog/activities/MainActivity.kt
+++ app/src/main/java/com/example/filmcatalog/activities/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.filmcatalog.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.filmcatalog.R
 import com.example.filmcatalog.databinding.ActivityMainBinding
+import com.example.filmcatalog.entities.Student
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,7 +13,16 @@ class MainActivity : AppCompatActivity() {
 
         val binding =  ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
-
         setContentView(view)
+
+        binding.mainActivityTextview.setOnClickListener { moveToNextScreen() }
+    }
+
+    private fun moveToNextScreen() {
+        intent = Intent(this, SecondActivity::class.java)
+        val student: Student = Student("Ilyas", 22)
+        intent.putExtra(SecondActivity.STUDENT, student)
+
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/filmcatalog/activities/SecondActivity.kt
+++ app/src/main/java/com/example/filmcatalog/activities/SecondActivity.kt
@@ -3,6 +3,7 @@ package com.example.filmcatalog.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.filmcatalog.databinding.ActivitySecondBinding
+import com.example.filmcatalog.entities.Student
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,7 +11,14 @@ class SecondActivity : AppCompatActivity() {
 
         val binding =  ActivitySecondBinding.inflate(layoutInflater)
         val view = binding.root
-
         setContentView(view)
+
+        val student: Student? = intent.getParcelableExtra<Student>(SecondActivity.STUDENT)
+
+        binding.secondActivityTextview.text = "Student name: ${student?.name}, student age: ${student?.age}"
+    }
+
+    companion object {
+        const val STUDENT = "student"
     }
 }
