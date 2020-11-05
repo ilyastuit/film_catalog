114d1204f8b726fff0d357f05bb7858f3e80bad5
ilyastuit <ilyastuit@gmail.com>
2020-11-05 13:13:40 +0300

    Added logging to all lifecycle activity methods on MainActivity

--- app/src/main/java/com/example/filmcatalog/MainActivity.kt
+++ app/src/main/java/com/example/filmcatalog/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.filmcatalog
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Timber.i("onStart Called")
+
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Timber.i("onResume Called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Timber.i("onPause Called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Timber.i("onStop Called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Timber.i("onDestroy Called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Timber.i("onRestart Called")
     }
 }
