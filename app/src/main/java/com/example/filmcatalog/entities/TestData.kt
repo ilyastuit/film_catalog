5e6bf2dd67f54a34ca5b379c745b2785be123dd0
ilyastuit <ilyastuit@gmail.com>
2020-11-05 20:47:41 +0300

    Added TestData

--- app/src/main/java/com/example/filmcatalog/entities/TestData.kt
+++ app/src/main/java/com/example/filmcatalog/entities/TestData.kt
@@ -1,2 +1,13 @@
 package com.example.filmcatalog.entities
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class TestData(
+        val transmittedString: String = "String to transmit",
+
+        val transmittedInt: Int = 12,
+
+        val transmittedBoolean: Boolean = false,
+) : Parcelable
