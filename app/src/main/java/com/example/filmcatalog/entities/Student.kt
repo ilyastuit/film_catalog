68a13fc08c8c6cbe870701c36740b3b30d2d19f0
ilyastuit <ilyastuit@gmail.com>
2020-11-05 21:41:36 +0300

    Creates SEcondActivity and Student entity

--- app/src/main/java/com/example/filmcatalog/entities/Student.kt
+++ app/src/main/java/com/example/filmcatalog/entities/Student.kt
@@ -1,2 +1,29 @@
 package com.example.filmcatalog.entities
 
+import android.os.Parcel
+import android.os.Parcelable
+
+class Student(
+    val name: String,
+    val age: Int) : Parcelable {
+
+    companion object {
+        @JvmField
+        val CREATOR = object : Parcelable.Creator<Student> {
+            override fun createFromParcel(parcel: Parcel) = Student(parcel)
+            override fun newArray(size: Int) = arrayOfNulls<Student>(size)
+        }
+    }
+
+    private constructor(parcel: Parcel) : this(
+        name = parcel.readString().toString(),
+        age = parcel.readInt()
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(name)
+        parcel.writeInt(age)
+    }
+
+    override fun describeContents() = 0
+}
