cc505411968b7c2e9785cf63b88eea72f0f208ab
ilyastuit <ilyastuit@gmail.com>
2020-11-05 23:00:56 +0300

    Changed activities to view binding

--- app/src/main/java/com/example/filmcatalog/activities/MainActivity.kt
+++ app/src/main/java/com/example/filmcatalog/activities/MainActivity.kt
@@ -3,10 +3,13 @@ package com.example.filmcatalog.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.filmcatalog.R
+import com.example.filmcatalog.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
--- app/src/main/java/com/example/filmcatalog/activities/ProfileActivity.kt
+++ app/src/main/java/com/example/filmcatalog/activities/ProfileActivity.kt
@@ -2,10 +2,13 @@ package com.example.filmcatalog.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.filmcatalog.databinding.ActivityProfileBinding
 
 class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        val binding = ActivityProfileBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
