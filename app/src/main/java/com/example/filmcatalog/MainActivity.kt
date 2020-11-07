f100f303bb421c0d636afb086a7eeed91914ae90
ilyastuit <ilyastuit@gmail.com>
2020-11-07 19:54:42 +0300

    Edited texts by using kotlin synthetic

--- app/src/main/java/com/example/filmcatalog/MainActivity.kt
+++ app/src/main/java/com/example/filmcatalog/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.filmcatalog
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        first_text.text = "First text."
+        second_text.text = "Second text."
+        third_text.text = "Third text."
+        fourth_text.text = "Fourth text."
+        fifth_text.text = "Fifth text."
     }
 }
