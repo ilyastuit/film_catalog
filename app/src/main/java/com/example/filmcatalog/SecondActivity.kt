6488187f241de4df302de0dc2868bf6b846eb1e1
ilyastuit <ilyastuit@gmail.com>
2020-11-07 20:05:04 +0300

    Changed second activity text with view binding

--- app/src/main/java/com/example/filmcatalog/SecondActivity.kt
+++ app/src/main/java/com/example/filmcatalog/SecondActivity.kt
@@ -2,12 +2,15 @@ package com.example.filmcatalog
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.filmcatalog.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
+        val binding = ActivitySecondBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-
+        binding.firstText.text = "Second Activity text"
     }
 }
